43de39574868c9a9b7aa3f644e2c307733e82eae
gonzalodr <129683931+gonzalodr@users.noreply.github.com>
2025-05-16 09:56:35 -0600

    add a daos student with crud for room
    - add daostudents with crud

--- app/src/main/java/com/moviles/courses/Daos/DaosStudent.kt
+++ app/src/main/java/com/moviles/courses/Daos/DaosStudent.kt
@@ -1,2 +1,24 @@
 package com.moviles.courses.Daos
 
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.moviles.courses.RoomEntity.StudentEntity
+
+@Dao
+interface DaosStudent {
+
+    @Query("SELECT * FROM students")
+    suspend fun getAllStudents(): List<StudentEntity>
+
+    @Query("SELECT * FROM students WHERE id = :id LIMIT 1")
+    suspend fun getStudentById(id: Int): StudentEntity
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(students: List<StudentEntity>)
+
+    @Query("DELETE FROM students")
+    suspend fun clearAll()
+}
