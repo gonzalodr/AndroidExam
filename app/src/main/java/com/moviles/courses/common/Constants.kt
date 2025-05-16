59a897d87ede1bbdb68824b959565ba1a80db78c
gonzalodr <129683931+gonzalodr@users.noreply.github.com>
2025-05-16 14:58:21 -0600

    add a constants ips

--- app/src/main/java/com/moviles/courses/common/Constants.kt
+++ app/src/main/java/com/moviles/courses/common/Constants.kt
@@ -1,1 +1,20 @@
 package com.moviles.courses.common
+
+object Constants {
+    private const val IS_PROD   = false
+    private const val DEV_IP    = "192.168.1.98" //ip of the computer remember to change it
+    private const val PROD_IP   = "api.miapp.com"
+    private const val PORT      = "5275"
+
+    val API_BASE_URL = if (IS_PROD) {
+        "https://$PROD_IP/"
+    } else {
+        "http://$DEV_IP:$PORT/"
+    }
+
+    val IMAGES_URL = if(IS_PROD){
+        "https://$PROD_IP/uploads/"
+    }else{
+        "http://$DEV_IP:$PORT/uploads/"
+    }
+}
