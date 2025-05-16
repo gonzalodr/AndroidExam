55900c82d8821bfb60225f6d86147006008c6021
gonzalodr <129683931+gonzalodr@users.noreply.github.com>
2025-05-16 09:47:43 -0600

    add student entities and add entity mapping entities to entity room
    - MapperStudent
    - Entity
    - Entity Room

--- app/src/main/java/com/moviles/courses/MappersEntity/MappersStundent.kt
+++ app/src/main/java/com/moviles/courses/MappersEntity/MappersStundent.kt
@@ -1,2 +1,21 @@
 package com.moviles.courses.MappersEntity
 
+
+import com.moviles.courses.RoomEntity.StudentEntity
+import com.moviles.courses.models.Student
+
+fun Student.toEntity() = StudentEntity(
+    id = this.id ?: 0,
+    name = this.name,
+    email = this.email,
+    phone = this.phone,
+    courseId = this.courseId
+)
+
+fun StudentEntity.toModel() = Student(
+    id = this.id,
+    name = this.name,
+    email = this.email,
+    phone = this.phone,
+    courseId = this.courseId
+)
--- app/src/main/java/com/moviles/courses/RoomEntity/StudentEntity.kt
+++ app/src/main/java/com/moviles/courses/RoomEntity/StudentEntity.kt
@@ -1,1 +1,14 @@
 package com.moviles.courses.RoomEntity
+
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "students")
+data class StudentEntity(
+    @PrimaryKey val id: Int,
+    val name: String,
+    val email: String,
+    val phone: String,
+    val courseId: Int
+)
