dfd445a5bf56a866dd3c3288e18f78d3279ed44a
DavidPMCR <alleriaysebastian@gmail.com>
2025-05-16 19:19:07 -0600

    files course
    Update files course

--- app/src/main/java/com/moviles/courses/network/RetrofitInstance.kt
+++ app/src/main/java/com/moviles/courses/network/RetrofitInstance.kt
@@ -1,1 +1,15 @@
 package com.moviles.courses.network
+
+import retrofit2.Retrofit
+import com.moviles.courses.common.Constants.API_BASE_URL
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    val api: ApiServices by lazy{
+        Retrofit.Builder()
+            .baseUrl(API_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiServices::class.java)
+    }
+}
